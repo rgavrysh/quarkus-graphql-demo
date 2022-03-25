1b93905ae0d18ca0ffd551d384f97404f7cf1e61
Roman Gavryshkiv <grome365@gmail.com>
2022-03-25 20:00:08 +0200

    hide java17 record. graalvm-ce-java17-21.3.0 native does not work with java17 Records

--- src/main/java/org/graphql/dom/Employee.java
+++ src/main/java/org/graphql/dom/Employee.java
@@ -1,3 +1,20 @@
 package org.graphql.dom;
 
-public record Employee(String id, String name, String role) {}
+import lombok.Getter;
+import lombok.Setter;
+
+// graalvm-ce-java17-21.3.0 native does not work with java17 Records
+//public record Employee(String id, String name, String role) {}
+@Getter
+@Setter
+public class Employee {
+  private String id;
+  private String name;
+  private String role;
+
+  public Employee(String id, String name, String role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+  }
+}
